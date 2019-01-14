86bea5994231021eded8e23ff2185e61175f4ece
Luca Ponzoni <ponzoniluca@gmail.com>
2019-01-14 16:30:30 -0500

    config.sh creates empty config.txt file

--- src/bin/config.sh
+++ src/bin/config.sh
@@ -5,11 +5,31 @@ set -e
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 WWW_DIR="${BIN_DIR%/src/bin}"
 
+# create config.txt file
+cd ${WWW_DIR}
+
+if [ -e config.txt ]
+then
+  cp -f config.txt{,.bak}
+  echo "Original file config.txt backed up as config.txt.bak"
+fi
+
+cat << EOF > config.txt
+#!/bin/bash
+set -e
+
+# At least Python 3.7 is required
+PYTHON="python"
+EOF
+
+
 # create config.py file
+cd ${WWW_DIR}/src/python
+
 if [ -e config.py ] 
 then 
   cp -f config.py{,.bak} 
-  echo "Original file config.py backed up as config.py.bak"
+  echo "Original file src/python/config.py backed up as src/python/config.py.bak"
 fi
 cat << EOF > ${WWW_DIR}/src/python/config.py
 PICKLES_DIR  = '${WWW_DIR}/workspace/pickles'
@@ -19,5 +39,10 @@ MAIN_CLSF    = '???'
 AUX_CLSF     = '???'
 EOF
 
-echo "An empty configuration file has been created"
-echo "Please set the requested paths in ${WWW_DIR}/script/python/config.py"
+echo
+echo "Clean configuration files have been created."
+echo "Please set the requested paths in:"
+echo ${WWW_DIR}/config.txt 
+echo "and:" 
+echo ${WWW_DIR}/src/python/config.py
+echo
