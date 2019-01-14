e5d9037ed82f0bf9ec6b13c9be18e3548a969dda
Luca Ponzoni <lponzoni@prody.cdb.pitt.edu>
2019-01-14 13:12:53 -0500

    added backup of config.py to config.sh

--- src/bin/config.sh
+++ src/bin/config.sh
@@ -6,6 +6,11 @@ BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 WWW_DIR="${BIN_DIR%/src/bin}"
 
 # create config.py file
+if [ -e config.py ] 
+then 
+  cp -f config.py{,.bak} 
+  echo "Original file config.py backed up as config.py.bak"
+fi
 cat << EOF > ${WWW_DIR}/src/python/config.py
 PICKLES_DIR  = '${WWW_DIR}/workspace/pickles'
 PRODY_DIR    = '???'
@@ -14,5 +19,5 @@ MAIN_CLSF    = '???'
 AUX_CLSF     = '???'
 EOF
 
-echo "An empty configuration file has been created."
+echo "An empty configuration file has been created"
 echo "Please set the requested paths in ${WWW_DIR}/script/python/config.py"
