4b25ed83edeece624ce360ce04401574941afe0c
luponzo86 <ponzoniluca@gmail.com>
2019-06-25 11:51:27 -0400

    test_locally script

--- src/bin/test_locally.sh
+++ src/bin/test_locally.sh
@@ -1,3 +1,11 @@
-cd ../..
+#!/bin/bash
+set -e
+
+# get correct path to website directory
+BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WWW_DIR="${BIN_DIR%/src/bin}"
+
+cd $WWW_DIR
+
 php -S localhost:8999 &
-firefox --new-window http://localhost:8999/
+firefox --new-window http://localhost:8999/ &
