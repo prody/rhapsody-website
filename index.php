99a301b8600e7f96fd90657585a0ae902ac0485f
luponzo86 <ponzoniluca@gmail.com>
2019-07-25 11:06:22 -0400

    new batch query thumbnail and index structure

--- index.php
+++ index.php
@@ -24,36 +24,56 @@
 
   <div class="container-fluid md-3 text-center">
     <div class="row">
-      <div class="col-md py-4">
+      <div class="col-md py-2">
         <h5><i>In silico</i> saturation mutagenesis </h5>
-        <div class="py-2">
-          <a href="sat_mutagen.php">
-            <img src="./img/thumbnail-sm.png"
-            class="img-responsive border rounded" style="width:80%" alt="">
-          </a>
-        </div>
-        <a href="./results.php?id=example-sm">see example</a>
+        <small class="form-text text-muted px-5">
+          Perform a complete scanning of all possible 19 amino acid
+          substitutions at every position on the sequence. See example:
+          <a href="./results.php?id=example-sm">human RAS</a>.
+        </small>
       </div>
-      <div class="col-md py-4">
-        <h5>Batch query of mixed variants </h5>
-        <div class="py-2">
-          <a href="batch_query.php">
-            <img src="./img/thumbnail-bq.png"
-            class="img-responsive border rounded" style="width:80%" alt="">
-          </a>
-        </div>
-        <a href="./results.php?id=example-bq">see example</a>
-      </div>
-      <div class="col-md py-4">
+
+      <div class="col-md py-2">
         <h5>Variants on custom PDB structure </h5>
-        <div class="py-2">
-          <a href="sat_mutagen.php">
-            <img src="./img/thumbnail-customPDB.jpg"
-            class="img-responsive border rounded" style="width:80%" alt="">
-          </a>
-        </div>
-        <!-- <a href="#">see example</a> -->
+        <small class="form-text text-muted px-5">
+          Perform saturation mutagenesis analysis on a specific conformer,
+          homology model or homologous structure (in case human structure is
+          not available).
+        </small>
       </div>
+
+      <div class="col-md py-2">
+        <h5>Batch query of mixed variants </h5>
+        <small class="form-text text-muted px-5">
+          Obtain predictions for a list of human missense variants from
+          various protein sequences. See an example
+          <a href="./results.php?id=example-bq">here</a>.
+        </small>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md py-2">
+        <a href="sat_mutagen.php">
+          <img src="./img/thumbnail-sm.png"
+          class="img-responsive border rounded" style="width:70%" alt="">
+        </a>
+      </div>
+
+      <div class="col-md py-2">
+        <a href="sat_mutagen.php">
+          <img src="./img/thumbnail-customPDB.png"
+          class="img-responsive border rounded" style="width:70%" alt="">
+        </a>
+      </div>
+
+      <div class="col-md py-2">
+        <a href="batch_query.php">
+          <img src="./img/thumbnail-bq.png"
+          class="img-responsive border rounded" style="width:70%" alt="">
+        </a>
+      </div>
+
     </div>
   </div>
 
