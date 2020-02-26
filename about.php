d47bf4de9a02e3348be794533b07768ce93c1f35
Luca Ponzoni <ponzoniluca@gmail.com>
2020-02-26 13:31:27 -0500

    add advanced access paper ref

--- about.php
+++ about.php
@@ -3,6 +3,7 @@
 
 <head>
 <?php readfile("./html/header.html"); ?>
+
 </head>
 
 <body>
@@ -23,23 +24,29 @@
 
       <div class="col-md-6">
         <h5>please cite:</h5>
+
         <p>
-        <a href="https://www.pnas.org/content/115/16/4164" target="_blank">
-          <i>Structural dynamics is a determinant of the functional
-          significance of missense variants </i><br>
-        </a>
-        Luca Ponzoni, Ivet Bahar <br>
-        <small>PNAS Apr 2018, 115 (16) 4164-4169;
-          DOI: 10.1073/pnas.1715896115 </small>
+        <div class="altmetric-embed" data-altmetric-id="76699800">preprint</div>
+        <a href="https://academic.oup.com/bioinformatics/advance-article/doi/10.1093/bioinformatics/btaa127/5758260?guestAccessKey=3da7600f-7185-43dd-b4a4-f647ad91d9f4" target="_blank">
+        <i>Rhapsody: Predicting the pathogenicity of human missense variants</i>
+        </a><br>
+        Luca Ponzoni, Daniel A. Peñaherrera, Zoltán N. Oltvai, Ivet Bahar<br>
+        <small>
+        <b>Bioinformatics</b> (in press, 26 Feb 2020), DOI: 10.1093/bioinformatics/btaa127<br>
+        [<a href="https://www.biorxiv.org/content/10.1101/737429v1" target="_blank">
+        bioRxiv preprint</a>]
+        </small>
         </p>
 
         <p>
-        <a href="https://www.biorxiv.org/content/10.1101/737429v1" target="_blank">
-          [preprint] <i>Rhapsody: Pathogenicity prediction of human missense variants
-            based on protein sequence, structure and dynamics</i><br>
-        </a>
-        Luca Ponzoni, Zoltan N. Oltvai, Ivet Bahar <br>
-        <small>bioRxiv Aug 2019, 737429; DOI: 10.1101/737429 </small>
+        <div class="altmetric-embed" data-altmetric-id="35144492"></div>
+        <a href="https://www.pnas.org/content/115/16/4164" target="_blank">
+          <i>Structural dynamics is a determinant of the functional
+          significance of missense variants </i>
+        </a><br>
+        Luca Ponzoni, Ivet Bahar <br>
+        <small><b>PNAS</b> Apr 2018, 115 (16) 4164-4169;
+          DOI: 10.1073/pnas.1715896115 </small>
         </p>
 
         <h5>other:</h5>
@@ -59,7 +66,9 @@
 
 <?php readfile("./html/footer.html"); ?>
 <?php readfile("./html/js_src.html"); ?>
+<script 
+  type="text/javascript" src="https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js">
+</script>
 
 </body>
 
-</html>
